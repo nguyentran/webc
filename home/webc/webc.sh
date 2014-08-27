d16076cdc025156ea3c5cd9d1553d216bf3747d1
root <root@debian>
2014-08-27 12:48:59 +0100

    copy homepage to /boot

--- home/webc/webc.sh
+++ home/webc/webc.sh
@@ -69,20 +69,27 @@ done
 #hompage=`cat homepage.txt`
 #rm -rf homepage.txt
 
-config_url="http://10.0.100.110/WebConvergerConfig/Default.aspx"
-#mac=$( mac_address )
-
-if curl -f -o /home/webc/homepage.tmp --retry 3 "$config_url?V=$webc_version&D=$device&K=$kernel&M=`mac_address`"
+if test -f /boot/homepage.cfg
 then
-        touch /home/webc/homepage.tmp
+	homepage=`cat /boot/homepage.cfg`
 else
-        echo "CONFIG: Failed to download from $config_url"
-	homepage="file:///etc/webc/homagepage_error.html" # default homepage
+	homepage="$install_qa_url"
 fi
 
+#config_url="http://10.0.100.110/WebConvergerConfig/Default.aspx"
+#mac=$( mac_address )
+
+#if curl -f -o /home/webc/homepage.tmp --retry 3 "$config_url?V=$webc_version&D=$device&K=$kernel&M=`mac_address`"
+#then
+#        touch /home/webc/homepage.tmp
+#else
+#        echo "CONFIG: Failed to download from $config_url"
+#	homepage="file:///etc/webc/homagepage_error.html" # default homepage
+#fi
+
 #homepage="$install_qa_url" # default homepage
-homepage=`cat /home/webc/homepage.tmp`
-rm -rf /home/webc/homepage.tmp
+#homepage=`cat /home/webc/homepage.tmp`
+#rm -rf /home/webc/homepage.tmp
 
 mkfifo "$live_config_pipe"
 read answer < "$live_config_pipe" # blocking till live-config is finished
