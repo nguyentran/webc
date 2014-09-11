48d6b12f5e2cf36abe1e288ee0c6998850bc1958
root <root@debian>
2014-09-11 13:24:34 +0100

    add new binary

--- etc/webc/vncserver.sh
+++ etc/webc/vncserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Xvnc :1 -desktop debian:1 -auth /webc/.Xauthority -geometry 1024x768 -depth 16 -rfbwait 30000 -rfbauth /home/webc/.vnc/passwd -rfbport 5901 -pn -fp /usr/X11R6/lib/X11/fonts/Type1/,/usr/X11R6/lib/X11/fonts/Speedo/,/usr/X11R6/lib/X11/fonts/misc/,/usr/X11R6/lib/X11/fonts/75dpi/,/usr/X11R6/lib/X11/fonts/100dpi/,/usr/share/fonts/X11/misc/,/usr/share/fonts/X11/Type1/,/usr/share/fonts/X11/75dpi/,/usr/share/fonts/X11/100dpi/ -co /etc/X11/rgb
+Xvnc4 :1 -desktop debian:1 -auth /home/webc/.Xauthority -geometry 1024x768 -depth 16 -rfbwait 30000 -rfbauth /home/webc/.vnc/passwd -rfbport 5901 -pn -fp /usr/X11R6/lib/X11/fonts/Type1/,/usr/X11R6/lib/X11/fonts/Speedo/,/usr/X11R6/lib/X11/fonts/misc/,/usr/X11R6/lib/X11/fonts/75dpi/,/usr/X11R6/lib/X11/fonts/100dpi/,/usr/share/fonts/X11/misc/,/usr/share/fonts/X11/Type1/,/usr/share/fonts/X11/75dpi/,/usr/share/fonts/X11/100dpi/ -co /etc/X11/rgb
 
