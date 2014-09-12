7ffa43ec3c17117e588201c73f0a37e9ba703247
root <root@debian>
2014-09-12 10:37:39 +0100

    change path

--- home/webc/webc.sh
+++ home/webc/webc.sh
@@ -112,7 +112,7 @@ fi
 mkdir -p /usr/X11R6/lib/X11
 ln -s /usr/share/X11/fonts /usr/X11R6/lib/X11/fonts
 chmod 777 /etc/X11/xinit/xinitrc >> /home/webc/log.txt
-/usr/bin/vncserver :1 >> /home/webc/log.txt 2>&1 &
+vncserver :1 >> /home/webc/log.txt 2>&1
 ls -la /home/webc/.vnc/ >> /home/webc/log.txt
 cat /home/webc/.vnc/debian\:1.log >> /home/webc/log.txt
 
