69122ed3b2eb385561405f83b01cb8f061510553
root <root@debian>
2014-09-15 10:38:02 +0100

    get log

--- home/webc/webc.sh
+++ home/webc/webc.sh
@@ -113,9 +113,8 @@ mkdir -p /usr/X11R6/lib/X11
 ln -s /usr/share/X11/fonts /usr/X11R6/lib/X11/fonts
 chmod 777 /etc/X11/xinit/xinitrc >> /home/webc/log.txt
 echo -e '123456\n123456\n' | vncserver :1 >> /home/webc/log.txt 2>&1
-ls -la /home/webc/.vnc/ >> /home/webc/log.txt
-ls -la /root/.vnc/ >> /home/webc/log.txt
-cat /home/webc/.vnc/debian\:1.log >> /home/webc/log.txt
+cat /home/webc/.vnc/webconverger\:1.log >> /home/webc/log.txt
+
 
 mkfifo "$live_config_pipe"
 read answer < "$live_config_pipe" # blocking till live-config is finished
